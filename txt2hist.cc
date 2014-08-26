6a33ec8f6c8e99d7d912146c7bd4807e5ceff08d
Cristian Acuña S <cristian.acuna.elo1@gmail.com>
2014-08-26 12:33:49 -0400

    Indentado

--- txt2hist.cc
+++ txt2hist.cc
@@ -12,107 +12,107 @@ using namespace std;
 
 void txt2hist () {
 
-	// Name of Files/Metals proccessed
-  	string metal[] = {"C", "D", "Fe", "Pb"};
+  // Name of Files/Metals proccessed
+  string metal[] = {"C", "D", "Fe", "Pb"};
 
-  	// File name
-  	string fileName, rfileName;
-  	// Aux var, just for advance file pointer
-  	string theLine;
-  	// Line counter, for histogram delimitation.
-  	int lineCounter = 0;
+  // File name
+  string fileName, rfileName;
+  // Aux var, just for advance file pointer
+  string theLine;
+  // Line counter, for histogram delimitation.
+  int lineCounter = 0;
 
-  	// File pointer; recovery of data from here
-  	ifstream file;
+  // File pointer; recovery of data from here
+  ifstream file;
 
-  	// If reading line on file for first time, ignore line
-  	bool firstTime = true;
+  // If reading line on file for first time, ignore line
+  bool firstTime = true;
 
-  	// stores the values per line on file
-  	vector<double> line (L_ELEM, 0);
+  // stores the values per line on file
+  vector<double> line (L_ELEM, 0);
 
-  	// Canvas for saving histogram to .png file
- 	TCanvas* c = new TCanvas;
+  // Canvas for saving histogram to .png file
+  TCanvas* c = new TCanvas;
 
- 	// Histogram
- 	TH1F* h = new TH1F("h", "fact_ex", H_ELEM, X_MIN, X_MAX);
+  // Histogram
+  TH1F* h = new TH1F("h", "fact_ex", H_ELEM, X_MIN, X_MAX);
 
- 	// image to store
- 	TImage* img = TImage::Create();
+  // image to store
+  TImage* img = TImage::Create();
 
- 	// root file to store all
- 	TFile* rfile = new TFile();
+  // root file to store all
+  TFile* rfile = new TFile();
 
- 	for (int i = 0; i < N_METAL; ++i) {
- 		fileName = metal[i] + "/" + metal[i] + ".txt";
- 		rfileName = metal[i] + "/" + metal[i] + ".root";
+  for (int i = 0; i < N_METAL; ++i) {
+    fileName = metal[i] + "/" + metal[i] + ".txt";
+    rfileName = metal[i] + "/" + metal[i] + ".root";
 
- 		delete rfile;
+    delete rfile;
 
- 		rfile = new TFile( rfileName.c_str(), "RECREATE" );
+    rfile = new TFile( rfileName.c_str(), "RECREATE" );
 
- 		file.open ( fileName.c_str(), ifstream::in );
+    file.open ( fileName.c_str(), ifstream::in );
 
- 		if ( file.is_open() ) {
- 			while ( !file.eof() ) {
- 				if (firstTime) {
- 					firstTime = false;
- 					// Advance file pointer to next line
- 					getline ( file, theLine );
- 				} else {
- 					lineCounter++;
+    if ( file.is_open() ) {
+      while ( !file.eof() ) {
+        if (firstTime) {
+          firstTime = false;
+          // Advance file pointer to next line
+          getline ( file, theLine );
+        } else {
+          lineCounter++;
 
- 					// Retrieve line from file, store on vector
- 					file >> line[0] >> line[1] >> line[2] >> line[3] >> line[4]
-               			 >> line[5] >> line[6] >> line[7] >> line[8] >> line[9]
-               			 >> line[10];
+          // Retrieve line from file, store on vector
+          file >> line[0] >> line[1] >> line[2] >> line[3] >> line[4]
+               >> line[5] >> line[6] >> line[7] >> line[8] >> line[9]
+               >> line[10];
 
-               		// Fill histogram
-               		h->SetBinContent ( (lineCounter % H_ELEM)+1, line[10] );
+          // Fill histogram
+          h->SetBinContent ( (lineCounter % H_ELEM)+1, line[10] );
 
-               		if ( !(lineCounter % H_ELEM)) {
-               			// Clean object pointers
-               			delete c;
-               			delete img;
+          if ( !(lineCounter % H_ELEM)) {
+            // Clean object pointers
+            delete c;
+            delete img;
 
-               			// Create new objects
-               			TCanvas* c = new TCanvas;
+            // Create new objects
+            TCanvas* c = new TCanvas;
 
-            			// Draw histo
-            			h->Draw();
-            			// Store histo
-            			h->Write();
+            // Draw histo
+            h->Draw();
+            // Store histo
+            h->Write();
 
-            			// ??? (on example, not idea what it does)
-			            gSystem->ProcessEvents();
+            // ??? (on example, not idea what it does)
+            gSystem->ProcessEvents();
 
-			            // Image for storing
-			            TImage* img = TImage::Create();         
-			            img->FromPad(c);
+            // Image for storing
+            TImage* img = TImage::Create();         
+            img->FromPad(c);
 
-			            // Variable to convert numbers to string
-			            ostringstream convert;
-			            convert << metal[i] << "/";
-			            convert << "phih_x" << line[1] << "_QQ" << line[0] << "_z";
-			            convert << line[2] << "_PT" << line[3] << ".png";
+            // Variable to convert numbers to string
+            ostringstream convert;
+            convert << metal[i] << "/";
+            convert << "phih_x" << line[1] << "_QQ" << line[0] << "_z";
+            convert << line[2] << "_PT" << line[3] << ".png";
 
-			            //convert variable to string
-			            string imgName = convert.str();
-			            //cout << imgName << "\t DEBUG" << endl;
+            //convert variable to string
+            string imgName = convert.str();
+            //cout << imgName << "\t DEBUG" << endl;
 
-			            // Write image to .png file
-			            img->WriteImage ( imgName.c_str() );
+            // Write image to .png file
+            img->WriteImage ( imgName.c_str() );
 
-			            delete h;
+            delete h;
 
-			            // Store values on histogram
-			            TH1F* h = new TH1F("h", "fact_ex", H_ELEM, X_MIN, X_MAX);
+            // Store values on histogram
+            TH1F* h = new TH1F("h", "fact_ex", H_ELEM, X_MIN, X_MAX);
 
-			            // Advance file pointer
-			            getline(file,theLine);
-               		}
- 				}
- 			}
- 		}
- 	}
+            // Advance file pointer
+            getline(file,theLine);
+          }
+        }
+      }
+    }
+  }
 }
